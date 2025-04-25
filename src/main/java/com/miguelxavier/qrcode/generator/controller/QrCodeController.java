63ad0bed2cda232620c9e64b78108aee492e0942
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-25 11:01:44 -0300

    feat: Add generate method to the QrCodeController

--- src/main/java/com/miguelxavier/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/miguelxavier/qrcode/generator/controller/QrCodeController.java
@@ -1,9 +1,32 @@
 package com.miguelxavier.qrcode.generator.controller;
 
+import com.miguelxavier.qrcode.generator.dto.QrCodeGenerateRequest;
+import com.miguelxavier.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.miguelxavier.qrcode.generator.service.QrCodeGenerateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/qrcode")
 public class QrCodeController {
+
+    private final QrCodeGenerateService qrCodeGenerateService;
+
+    public QrCodeController(QrCodeGenerateService qrCodeGenerateService) {
+        this.qrCodeGenerateService = qrCodeGenerateService;
+    }
+
+    @PostMapping
+    public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGenerateService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
+
+    }
 }
