74b0bf8a25d2ae14b6ef8be8c3651af7133b7488
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-25 10:25:45 -0300

    feat: Create QrCodeController

---
+++ src/main/java/com/miguelxavier/qrcode/generator/controller/QrCodeController.java
@@ -0,0 +1,9 @@
+package com.miguelxavier.qrcode.generator.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/qrcode")
+public class QrCodeController {
+}
